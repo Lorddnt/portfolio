b245364f20533737964cc2fe4b58d589add87317
Lorddnt <david.awoke@gmail.com>
2023-11-09 11:59:16 +0100

    I added contact form file

---
+++ validation.js
@@ -0,0 +1,21 @@
+function sendMail(){
+    var params = {
+        name:document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        subject:document.getElementById("subject").value,
+        message:document.getElementById("message").value
+    };
+
+const serviceID = "service_2wgzdvi";
+const templateID = "template_3jys4ct";
+
+emailjs.send(serviceID, templateID, params).then((res) =>{
+    document.getElementById("name").value ="";
+    document.getElementById("email").value ="";
+    document.getElementById("subject").value ="";
+    document.getElementById("message").value ="";
+    console.log(res);
+    alert("Your message has been sent sucessfully!");
+})
+.catch((err) => console.log(err));
+}
